8222156c65853c6bb1da8ef7304f20ecb9c4d464
Yanko <105419760+YankoVlahov@users.noreply.github.com>
2022-11-21 10:28:05 +0200

    Add project files.

---
+++ WinFormsApp1/TestForm.cs
@@ -0,0 +1,36 @@
+using IronXL;
+
+namespace WinFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e  )
+        {
+            string path = @"C:\Users\YankoVlahov\Desktop\MovementsExport_2022-09-30_2022-10-07.csv";
+            // Create LoadOptions for CSV file
+            WorkBook workbook = WorkBook.LoadCSV(path, fileFormat: ExcelFileFormat.XLSX, ListDelimiter: ";");
+            //WorkSheet ws = workbook.DefaultWorkSheet;
+            using (SaveFileDialog openFileDialog = new SaveFileDialog())
+            {
+                openFileDialog.FileName = "CsvToExcelConversion.xlsx";
+                openFileDialog.ShowDialog();              
+                string  file = openFileDialog.FileName;              
+                workbook.SaveAs(file);
+                // (@"C:\CsvToExcelConversion.xlsx");
+                string getpath = @file + "CsvToExcelConversion.xlsx";
+                    
+                
+            }
+
+                
+            
+            
+            
+        }
+    }
+}
